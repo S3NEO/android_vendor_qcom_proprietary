4a6c7803ff266f8ff3a1a7da59b60043ede9946c
PythonLimited <Leonardomeitz@gmail.com>
2018-12-06 20:08:14 +0100

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,1 @@
+Products += libril-qc-qmi-1 rild libril netmgrd qmuxd libconfigdb libdiag libdsi_netctrl libdsutils libidl libnetmgr libqcci_legacy libqdi libqdp libqmi libqmi_cci libqmi_client_qmux libqmi_common libqmi_csi libqmi_encdec libqmiservices libril-qcril-hook-oem libxml time_daemon libtime_genoff libdsnetutils libdsucsd libqmi_common_so libqmi_csvt_srvc ATFWD-daemon ds_fmc_appd radish
